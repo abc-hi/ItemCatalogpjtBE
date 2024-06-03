780bcc795954dcafbea05f7ce364e5617d33db19
abc-hi <revathimohancse@gmail.com>
2024-06-03 00:11:04 -0700

    final commit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
-       return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+   cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     }
 });
 
