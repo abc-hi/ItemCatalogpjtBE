98ddfe41098bbfdcd5c02ec77cd632d86ac13f0e
abc-hi <revathimohancse@gmail.com>
2024-06-02 22:41:38 -0700

    add return to index

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
-        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+       return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     }
 });
 
