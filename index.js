4e657905aad172d11b40ae957fe631f1f713be0c
abc-hi <revathimohancse@gmail.com>
2024-08-01 23:50:19 -0700

    cors 4th

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ app.use(express.json());
 // }));
 
 app.use(cors({
-    origin: 'https://itemcatelogfrontcode.netlify.app/', 
+    origin: 'https://itemcatelogfrontcode.netlify.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Allow credentials to be included in requests
