500db7e973e8292f1ad47c0b80fb6f7f63d306c3
abc-hi <revathimohancse@gmail.com>
2024-08-02 00:24:22 -0700

    https added

--- index.js
+++ index.js
@@ -57,7 +57,7 @@
 // app.post("/upload", upload.single('product'), (req, res) => {
 //     res.json({
 //         success: 1,
-//         // image_url: `http://localhost:${port}/images/${req.file.filename}`
+//         // image_url: `https://localhost:${port}/images/${req.file.filename}`
 //                 image_url: `https://itemcatalogpjtbe.onrender.com/images/${req.file.filename}`
 
         
